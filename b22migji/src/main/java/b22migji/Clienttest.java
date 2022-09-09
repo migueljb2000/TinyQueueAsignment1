1be6a9a29da52e23aacc920d2380e107123bee0d
migue <migue@10.5.19.196>
2022-09-09 17:25:52 +0200

    Added Json library and functions for subscribe and login, started work on heartbeat

--- b22migji/src/main/java/b22migji/Clienttest.java
+++ b22migji/src/main/java/b22migji/Clienttest.java
@@ -4,6 +4,7 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Socket;
+import org.json.*;
 
 public class Clienttest {
 	public static void main(String [] args) {
@@ -11,17 +12,61 @@ public class Clienttest {
 		if (args.length > 0) {
 			try (ZContext context = new ZContext()){
 				Socket socket = context.createSocket(SocketType.DEALER);
-				socket.connect(args[0]);
-				String msg = "{\"subscribe\":true}";
-				socket.send(msg);
 				
-				byte[] response = socket.recv();
-				System.out.println("Reply from server " + new String(response, ZMQ.CHARSET));
+				System.out.println(joinqueue(socket, args, "miguel"));
+				System.out.println(subscribe(socket, args));
+				//heartbeat(socket, args);
 			} ;
 		}else {
 			System.out.println("Please specify an url");
 		}
 		
+	}
+	
+	public static String subscribe(Socket socket, String [] args) {
+		
+		socket.connect(args[0]);
+		JSONObject msg = new JSONObject();
+		msg.put("subscribe", "true");
+		socket.send(msg.toString());
+		byte[] response = socket.recv();
+		 
+		return new String(response, ZMQ.CHARSET);
+	}
+	
+	public static String joinqueue(Socket socket, String [] args, String name) {
+		
+		socket.connect(args[0]);
+		JSONObject login = new JSONObject();
+		login.put("enterQueue", "true");
+		login.put("name", name);
+		socket.send(login.toString());
+		byte[] ticket = socket.recv();
+		
+		return new String(ticket, ZMQ.CHARSET);
+		
+	}
+	
+	public static void heartbeat(Socket socket, String [] args) {
+		/* wait is giving java.lang.IllegalMonitorStateException: current thread is not owner
+		*  I'm trying to set up the heartbeat signal so it gets from the server the period 
+		*  between heartbeats and it waits that amount of time. 
+		*  
+		*  Waiting for the server to send a heartbeat to send our heartbeat would be better 
+		*  but I haven't seen a specific method for that
+		*
+		*/
+		Long time = (long) socket.getHeartbeatTtl();
+		byte[] heartbeat = socket.getHeartbeatContext();
+		while (1 < 2) {
+			socket.send("");
+			try {
+				socket.wait(4);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
